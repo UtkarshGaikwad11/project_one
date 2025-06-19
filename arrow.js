711d228851886f4bcf1d5c3fb6bf88a3b3928cd1
UtkarshGaikwad11 <108927005+UtkarshGaikwad11@users.noreply.github.com>
2025-06-19 19:34:35 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,27 @@
+// const home=function(){
+
+//     let useName="Rohit"
+//     console.log(useName)
+// }
+
+// const home=() => {
+
+//     let useName="Rohit"
+//     console.log(useName)
+// }
+//  home()
+
+// 
+
+
+
+console.log("++++++++++++++++++++IIFE+++++++++++++++")
+
+// ( (name) => {
+//     console.log(`DB connected ${name}`);
+// } )
+// (`Rahul`)
+
+((name) => {
+    console.log(`DB Connected ${name}`);
+})(`uttu`)
